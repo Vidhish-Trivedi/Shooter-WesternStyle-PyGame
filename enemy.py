0529decb828b3f5fdd644b5c5f6be9715b06d2f5
Vidhish Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2022-08-20 23:02:27 +0530

    Refactoring, Sharing methods between different enemies.
    Created a class Enemy(), from which both, the Cactus() and the Coffin() class inherit.

--- enemy.py
+++ enemy.py
@@ -1,8 +1,46 @@
 import pygame as pg
 from entity import Entity
 
+# Class for shared methods and attributes between monsters.
+class Enemy:
+    def get_player_dist_dir(self):
+        enemy_pos = pg.math.Vector2(self.rect.center)
+        player_pos = pg.math.Vector2(self.player.rect.center)
 
-class Coffin(Entity):
+        dist = (player_pos - enemy_pos).magnitude()
+        if(dist != 0):
+            dir = (player_pos - enemy_pos).normalize()  # magnitude = 1.
+        else:
+            dir = pg.math.Vector2(0, 0)
+
+        return (dist, dir)
+            
+    def face_player(self):
+        dist, dir = self.get_player_dist_dir()
+        if(dist < self.detect_radius):
+            if(dir.y > -0.5  and dir.y < 0.5):  # Detect whether player is above or below AND on left or right side.
+                if(dir.x < 0):  # Player is on left side.
+                    self.move_dir = "left_idle"
+                elif(dir.x > 0):  # Player is on right side.
+                    self.move_dir = "right_idle"
+
+            else:
+                if(dir.y < 0):  # Player is on top side.
+                    self.move_dir = "up_idle"
+                elif(dir.y > 0):  # Player is on bottom side.
+                    self.move_dir = "down_idle"
+
+    def walk_to_player(self):
+        dist, dir = self.get_player_dist_dir()
+        if(dist < self.move_to_player_radius and dist > self.attack_radius):
+            self.direction = dir  # Overwrite self.direction inside Entity() class.
+            self.move_dir = self.move_dir.split("_")[0]
+        
+        else:
+            self.direction = pg.math.Vector2()  # Stop enemy motion.
+
+
+class Coffin(Entity, Enemy):
     def __init__(self, position, groups, asset_path, coll_sprites, player_par):
         super().__init__(position, groups, asset_path, coll_sprites)
         
@@ -15,42 +53,6 @@ class Coffin(Entity):
         self.move_to_player_radius = 400
         self.attack_radius = 50
 
-    def get_player_dist_dir(self):
-        enemy_pos = pg.math.Vector2(self.rect.center)
-        player_pos = pg.math.Vector2(self.player.rect.center)
-
-        dist = (player_pos - enemy_pos).magnitude()
-        if(dist != 0):
-            dir = (player_pos - enemy_pos).normalize()  # magnitude = 1.
-        else:
-            dir = pg.math.Vector2(0, 0)
-
-        return (dist, dir)
-            
-    def face_player(self):
-        dist, dir = self.get_player_dist_dir()
-        if(dist < self.detect_radius):
-            if(dir.y > -0.5  and dir.y < 0.5):  # Detect whether player is above or below AND on left or right side.
-                if(dir.x < 0):  # Player is on left side.
-                    self.move_dir = "left_idle"
-                elif(dir.x > 0):  # Player is on right side.
-                    self.move_dir = "right_idle"
-
-            else:
-                if(dir.y < 0):  # Player is on top side.
-                    self.move_dir = "up_idle"
-                elif(dir.y > 0):  # Player is on bottom side.
-                    self.move_dir = "down_idle"
-
-    def walk_to_player(self):
-        dist, dir = self.get_player_dist_dir()
-        if(dist < self.move_to_player_radius and dist > self.attack_radius):
-            self.direction = dir  # Overwrite self.direction inside Entity() class.
-            self.move_dir = self.move_dir.split("_")[0]
-        
-        else:
-            self.direction = pg.math.Vector2()  # Stop enemy motion.
-
     def animate(self, deltaTime):
         self.frame_index += 7*deltaTime
 
@@ -59,59 +61,24 @@ class Coffin(Entity):
 
         self.image = self.animations[self.move_dir][int(self.frame_index)]
 
-
     def update(self, deltaTime):
         self.face_player()
         self.walk_to_player()
         self.move_entity(deltaTime)
         self.animate(deltaTime)
 
-class Cactus(Entity):
+class Cactus(Entity, Enemy):
     def __init__(self, position, groups, asset_path, coll_sprites, player_par):
         super().__init__(position, groups, asset_path, coll_sprites)
+
+        # Overwrites.
+        self.speed = 90
         
         # Player interactivity.
         self.player = player_par
-        self.detect_radius = 550
-        self.move_to_player_radius = 400
-        self.attack_radius = 50
-
-
-    def get_player_dist_dir(self):
-        enemy_pos = pg.math.Vector2(self.rect.center)
-        player_pos = pg.math.Vector2(self.player.rect.center)
-
-        dist = (player_pos - enemy_pos).magnitude()
-        if(dist != 0):
-            dir = (player_pos - enemy_pos).normalize()  # magnitude = 1.
-        else:
-            dir = pg.math.Vector2(0, 0)
-
-        return (dist, dir)
-            
-    def face_player(self):
-        dist, dir = self.get_player_dist_dir()
-        if(dist < self.detect_radius):
-            if(dir.y > -0.5  and dir.y < 0.5):  # Detect whether player is above or below AND on left or right side.
-                if(dir.x < 0):  # Player is on left side.
-                    self.move_dir = "left_idle"
-                elif(dir.x > 0):  # Player is on right side.
-                    self.move_dir = "right_idle"
-
-            else:
-                if(dir.y < 0):  # Player is on top side.
-                    self.move_dir = "up_idle"
-                elif(dir.y > 0):  # Player is on bottom side.
-                    self.move_dir = "down_idle"
-
-    def walk_to_player(self):
-        dist, dir = self.get_player_dist_dir()
-        if(dist < self.move_to_player_radius and dist > self.attack_radius):
-            self.direction = dir  # Overwrite self.direction inside Entity() class.
-            self.move_dir = self.move_dir.split("_")[0]
-        
-        else:
-            self.direction = pg.math.Vector2()  # Stop enemy motion.
+        self.detect_radius = 600
+        self.move_to_player_radius = 500
+        self.attack_radius = 350
 
     def animate(self, deltaTime):
         self.frame_index += 7*deltaTime
@@ -121,10 +88,8 @@ class Cactus(Entity):
 
         self.image = self.animations[self.move_dir][int(self.frame_index)]
 
-
     def update(self, deltaTime):
         self.face_player()
         self.walk_to_player()
         self.move_entity(deltaTime)
         self.animate(deltaTime)
-    
